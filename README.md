ab0c9bb3ae9119620d06bd367a24d750a9782bf2
Fatih Erikli <fatiherikli@gmail.com>
2020-04-01 21:44:13 +0200

    Add docs folder

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 The demo and the article can be found:
 
 <https://fatiherikli.github.io/coronavirus-simulation>
+
+![](https://raw.githubusercontent.com/fatiherikli/coronavirus-simulation/master/screenshot.gif)
---
+++ docs/precache-manifest.cc213bcf685fae242f5d41e9edc7c2ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85737ee0125e6c5937b904ebb229fadb",
+    "url": "/coronavirus-simulation/index.html"
+  },
+  {
+    "revision": "6bf07ddb249fabba8589",
+    "url": "/coronavirus-simulation/static/css/main.47e472f4.chunk.css"
+  },
+  {
+    "revision": "d03397fb1d226a624a97",
+    "url": "/coronavirus-simulation/static/js/2.2695d89b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coronavirus-simulation/static/js/2.2695d89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf07ddb249fabba8589",
+    "url": "/coronavirus-simulation/static/js/main.32b7f127.chunk.js"
+  },
+  {
+    "revision": "77e2921deadc088d248c",
+    "url": "/coronavirus-simulation/static/js/runtime-main.43b6624d.js"
+  }
+]);
